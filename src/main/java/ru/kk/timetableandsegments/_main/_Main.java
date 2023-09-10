0cf3bbb133e2f4b7f43b905d5f9ef851adfc5243
EJIeKTpa <sawwaksu@yandex.ru>
2023-09-10 23:38:01 +0900

    1

---
+++ src/main/java/ru/kk/timetableandsegments/_main/_Main.java
@@ -0,0 +1,11 @@
+package ru.kk.timetableandsegments._main;
+
+import ru.kk.timetableandsegments.service.Timetable;
+import ru.kk.timetableandsegments.service.Segments;
+
+public class _Main {
+    public static void main(String[] args) {
+        Timetable.timetable();
+        Segments.segments();
+    }
+}
