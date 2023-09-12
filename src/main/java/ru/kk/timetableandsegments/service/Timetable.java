131a7bb2e76dc0f1dd33e6c848cf59cc8c015791
EJIeKTpa <sawwaksu@yandex.ru>
2023-09-12 09:33:07 +0900

    1

--- src/main/java/ru/kk/timetableandsegments/service/Timetable.java
+++ src/main/java/ru/kk/timetableandsegments/service/Timetable.java
@@ -2,9 +2,10 @@ package ru.kk.timetableandsegments.service;
 
 public class Timetable {
     public static void timetable() {
-        int[] table = {1, 2, 3, 4, 5};
+        int[] table = {2, 3, 4, 5};
+        int[] table1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int i : table) {
-            for (int j : table) {
+            for (int j : table1) {
                 int result = i * j;
                 System.out.println(i + " * " + j + " = " + result);
             }
